ae8bffafb9503c65c0a80b6d77fce37a7e509185
Jeff Hutchins <jhutchins@getjive.com>
2013-09-10 14:17:27 -0600

    Add instance of Dfd on export object.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.Dfd = require('./promise.js')._u_.Dfd;
+module.exports.dfd = require('./promise.js')._u_.dfd;
 module.exports.State = require('./state.js')._u_.State;
 module.exports.Fabric = require('./fabric.js').Fabric;
