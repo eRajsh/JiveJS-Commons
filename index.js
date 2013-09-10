e965e35d29a1c75006af6ca2269879bdcc7fe349
Jeff Hutchins <jhutchins@getjive.com>
2013-09-10 09:52:38 -0600

    Update the jive object a little more.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-module.exports._u_ = {};
-module.exports._u_.Dfd = require('./promise.js')._u_.Dfd;
-module.exports._u_.State = require('./state.js')._u_.State;
+module.exports.Dfd = require('./promise.js')._u_.Dfd;
+module.exports.State = require('./state.js')._u_.State;
 module.exports.Fabric = require('./fabric.js').Fabric;
