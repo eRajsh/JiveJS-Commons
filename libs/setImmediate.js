964e2b8edd5e50d788a4190f3823f44b13f38cf0
Jessie A. Morris <jessie@jessieamorris.com>
2013-11-21 16:35:22 -0700

    Improve the performance of setImmediate.
    This makes setImmediate significantly faster than it was before. A lot
    less GC's and such occur in here now. Also, the "Arguments is invalid
    context" inside setImmediate has died.

--- libs/setImmediate.js
+++ libs/setImmediate.js
@@ -1,62 +1,45 @@
 (function (global, undefined) {
 	"use strict";
 
-	var tasks = (function () {
-		function Task(handler, args) {
-			this.handler = handler;
-			this.args = args;
-		}
-		Task.prototype.run = function () {
-			if (typeof this.handler === "function") {
-				this.handler.apply(undefined, this.args);
-			} else {
-				var scriptSource = "" + this.handler;
-				eval(scriptSource);
-			}
-		};
+	var nextHandle = 1; 
+	var freeHandle;
 
-		var nextHandle = 1; 
-		var tasksByHandle = {};
-		var currentlyRunningATask = false;
+	var cbsByHandle = [];
+	var argsByHandle = [];
 
+	var howMany = 0;
+
+	global.setImmediateDebug = function() {
 		return {
-			addFromSetImmediateArguments: function (args) {
-				var handler = args[0];
-				var argsToHandle = Array.prototype.slice.call(args, 1);
-				var task = new Task(handler, argsToHandle);
+			nextHandle: nextHandle,
+			freeHandle: freeHandle,
+			cbsByHandle: cbsByHandle,
+			argsByHandle: argsByHandle,
+			howMany: howMany
+		}
+	};
 
-				var thisHandle = nextHandle++;
-				tasksByHandle[thisHandle] = task;
-				return thisHandle;
-			},
-			runIfPresent: function (handle) {
-				if (!currentlyRunningATask) {
-					var task = tasksByHandle[handle];
-					if (task) {
-						currentlyRunningATask = true;
-						task.run();
-						delete tasksByHandle[handle];
-						currentlyRunningATask = false;
-					}
-				} else {
-					global.setTimeout(function () {
-						tasks.runIfPresent(handle);
-					}, 0);
-				}
-			},
-			remove: function (handle) {
-				delete tasksByHandle[handle];
-			}
-		};
-	}());
+	var handle;
+	function addFromSetImmediate(cb, args) {
+		handle = (freeHandle !== undefined) ? freeHandle : nextHandle++;
+		cbsByHandle[handle] = cb;
+		argsByHandle[handle] = args;
+
+		freeHandle = undefined;
+
+		return handle;
+	}
+
+	function runIfPresent(key) {
+		key = parseInt(key, 10);
+		cbsByHandle[key] ? cbsByHandle[key].apply(undefined, argsByHandle[key]) : false;
+		freeHandle = key;
+		howMany++;
+	}
 
 	function canUseNextTick() {
 		return typeof process === "object" &&
 			   Object.prototype.toString.call(process) === "[object process]";
-	}
-
-	function canUseMessageChannel() {
-		return !!global.MessageChannel;
 	}
 
 	function canUsePostMessage() {
@@ -75,48 +58,31 @@
 		return postMessageIsAsynchronous;
 	}
 
-	function canUseReadyStateChange() {
-		return "document" in global && "onreadystatechange" in global.document.createElement("script");
+	function canUseMessageChannel() {
+		return !!global.MessageChannel;
 	}
 
 	function installNextTickImplementation(attachTo) {
 		attachTo.setImmediate = function () {
-			var handle = tasks.addFromSetImmediateArguments(arguments);
+			var args = arguments;
 
 			process.nextTick(function () {
-				tasks.runIfPresent(handle);
+				args[0].call(undefined, Array.prototype.slice.call(args, 1));
 			});
-
-			return handle;
-		};
-	}
-
-	function installMessageChannelImplementation(attachTo) {
-		var channel = new global.MessageChannel();
-		channel.port1.onmessage = function (event) {
-			var handle = event.data;
-			tasks.runIfPresent(handle);
-		};
-		attachTo.setImmediate = function () {
-			var handle = tasks.addFromSetImmediateArguments(arguments);
-
-			channel.port2.postMessage(handle);
-
-			return handle;
 		};
 	}
 
 	function installPostMessageImplementation(attachTo) {
 		var MESSAGE_PREFIX = "com.setImmediate" + Math.random();
+		var MESSAGE_PREFIX_LENGTH = MESSAGE_PREFIX.length;
 
-		function isStringAndStartsWith(string, putativeStart) {
-			return typeof string === "string" && string.substring(0, putativeStart.length) === putativeStart;
+		function isStringAndStartsWith(string) {
+			return typeof string === "string" && string.substring(0, MESSAGE_PREFIX_LENGTH) === MESSAGE_PREFIX;
 		}
 
 		function onGlobalMessage(event) {
-			if (event.source === global && isStringAndStartsWith(event.data, MESSAGE_PREFIX)) {
-				var handle = event.data.substring(MESSAGE_PREFIX.length);
-				tasks.runIfPresent(handle);
+			if (event.source === global && isStringAndStartsWith(event.data)) {
+				runIfPresent(event.data.substring(MESSAGE_PREFIX_LENGTH));
 			}
 		}
 		if (global.addEventListener) {
@@ -126,41 +92,34 @@
 		}
 
 		attachTo.setImmediate = function () {
-			var handle = tasks.addFromSetImmediateArguments(arguments);
+			var cb = arguments[0];
+			var args = Array.prototype.slice.call(arguments, 1);
 
-			global.postMessage(MESSAGE_PREFIX + handle, "*");
-
-			return handle;
+			global.postMessage(MESSAGE_PREFIX + addFromSetImmediate(cb, args), "*");
 		};
 	}
 
-	function installReadyStateChangeImplementation(attachTo) {
+	function installMessageChannelImplementation(attachTo) {
+		var channel = new global.MessageChannel();
+		channel.port1.onmessage = function (event) {
+			runIfPresent(event.data);
+		};
 		attachTo.setImmediate = function () {
-			var handle = tasks.addFromSetImmediateArguments(arguments);
+			var cb = arguments[0];
+			var args = Array.prototype.slice.call(arguments, 1);
 
-			var scriptEl = global.document.createElement("script");
-			scriptEl.onreadystatechange = function () {
-				tasks.runIfPresent(handle);
-
-				scriptEl.onreadystatechange = null;
-				scriptEl.parentNode.removeChild(scriptEl);
-				scriptEl = null;
-			};
-			global.document.documentElement.appendChild(scriptEl);
-
-			return handle;
+			channel.port2.postMessage(addFromSetImmediate(cb, args));
 		};
 	}
 
 	function installSetTimeoutImplementation(attachTo) {
 		attachTo.setImmediate = function () {
-			var handle = tasks.addFromSetImmediateArguments(arguments);
+			var cb = arguments[0];
+			var args = Array.prototype.slice.call(arguments, 1);
 
 			global.setTimeout(function () {
-				tasks.runIfPresent(handle);
+				cb.call(undefined, args);
 			}, 0);
-
-			return handle;
 		};
 	}
 
@@ -169,22 +128,14 @@
 						  Object.getPrototypeOf(global)
 						: global;
 
-		if ((typeof self !== "undefined" && self.Jive && self.Jive.Features && self.Jive.Features.RetardMode) || (typeof Worker === "undefined" || typeof WebSocket === "undefined")) {
-			installSetTimeoutImplementation(attachTo);
+		if (canUseNextTick()) {
+			installNextTickImplementation(attachTo);
+		} else if (canUsePostMessage()) {
+			installPostMessageImplementation(attachTo);
+		} else if (canUseMessageChannel()) {
+			installMessageChannelImplementation(attachTo);
 		} else {
-			if (canUseNextTick()) {
-				installNextTickImplementation(attachTo);
-			} else if (canUsePostMessage()) {
-				installPostMessageImplementation(attachTo);
-			} else if (canUseMessageChannel()) {
-				installMessageChannelImplementation(attachTo);
-			} else if (canUseReadyStateChange()) {
-				installReadyStateChangeImplementation(attachTo);
-			} else {
-				installSetTimeoutImplementation(attachTo);
-			}
+			installSetTimeoutImplementation(attachTo);
 		}
-
-		attachTo.clearImmediate = tasks.remove;
 	}
 }(typeof self === "object" && self ? self : this));
